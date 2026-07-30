0c2b46f3104b7322cbf02d1656155bd14cab2b29
AbdallaZahra <abdallazahra436@gmail.com>
2026-07-30 11:14:23 +0300

    Update main.js

--- main.js
+++ main.js
@@ -294,7 +294,7 @@ document.addEventListener("DOMContentLoaded", () => {
   window.__bg_music__ = true;
 
   const music = document.createElement("audio");
-  music.src = "images/1025.MP3";
+  music.src = "images/1025.m4a";
   music.loop = true;
   music.volume = 0;
   document.body.appendChild(music);
